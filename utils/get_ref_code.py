98f7f0250a5c15bbf251d4cbb871b18ba30821b2
nazavod <91217742+nazavod777@users.noreply.github.com>
2023-12-17 18:29:30 +0300

    Update get_ref_code.py

--- utils/get_ref_code.py
+++ utils/get_ref_code.py
@@ -1,12 +1,4 @@
-from random import choice
-
-import aiofiles
-
-
-async def get_ref_code() -> str:
-    async with aiofiles.open(file='ref_codes.txt',
-                             mode='r',
-                             encoding='utf-8-sig') as file:
-        ref_codes: list[str] = [row.strip() for row in await file.readlines()]
-
-    return choice(ref_codes)
+with open(file='ref_codes.txt',
+          mode='r',
+          encoding='utf-8-sig') as file:
+    ref_codes: list[str] = [row.strip() for row in file]
